005597f36e2c114e1a55b99a1238e282d8161370
A1ist <66681496+A1ist@users.noreply.github.com>
2022-04-06 22:16:59 +0800

    Add files via upload

--- misc/utils.py
+++ misc/utils.py
@@ -1,3 +1,6 @@
+import six
+from six.moves import cPickle
+
 def if_use_feat(caption_model):
     if caption_model in ['show_tell', 'all_img', 'fc', 'newfc']:
         use_fc, use_att = True, False
@@ -8,3 +11,9 @@ def if_use_feat(caption_model):
     else:
         use_fc, use_att = False, True
     return use_fc, use_att
+
+def pickle_load(f):
+    if six.PY3:
+        return cPickle.load(f, encoding='latin-1')
+    else:
+        return cPickle.load(f)
