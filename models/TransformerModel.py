e02322a65fd50fd0fb81ff3ae190b7fc1c945213
A1ist <66681496+A1ist@users.noreply.github.com>
2022-04-08 22:26:01 +0800

    Add files via upload

--- models/TransformerModel.py
+++ models/TransformerModel.py
@@ -11,3 +11,17 @@ class LayerNorm(nn.Module):
 
 def clones(module, N):
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+class SubLayerConnection(nn.Module):
+    def __init__(self, size, dropout):
+        super(SubLayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
