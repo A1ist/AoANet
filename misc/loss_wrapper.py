918aaf7474f67d8f7afb7688a2af296805428265
A1ist <66681496+A1ist@users.noreply.github.com>
2022-04-10 22:10:26 +0800

    Add files via upload

---
+++ misc/loss_wrapper.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+import AOANet_C.misc.utils as utils
+
+class LossWrapper(nn.Module):
+    def __init__(self, model, opt):
+        super(LossWrapper, self).__init__()
+        self.opt = opt
+        self.model = model
+        if opt.label_smoothing > 0:
+            self.crit = utils.LabelSmoothing(smoothing=opt.label_smoothing)
+        else:
+            self.crit = utils.LanguageModelCriterion()
+        self.rl_crit = utils.RewardCriterion()
