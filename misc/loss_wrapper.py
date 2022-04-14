1d0227c3ef489a467d73ee31240d7578815fd384
A1ist <66681496+A1ist@users.noreply.github.com>
2022-04-14 22:03:30 +0800

    Add files via upload

--- misc/loss_wrapper.py
+++ misc/loss_wrapper.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import AOANet_C.misc.utils as utils
+import torch
 
 class LossWrapper(nn.Module):
     def __init__(self, model, opt):
@@ -11,3 +12,20 @@ class LossWrapper(nn.Module):
         else:
             self.crit = utils.LanguageModelCriterion()
         self.rl_crit = utils.RewardCriterion()
+    def forward(self, fc_feats, att_feats, labels, masks, att_masks, gts, gt_indices, sc_flag):
+        out = {}
+        if not sc_flag:
+            loss = self.crit(self.model(fc_feats, att_feats, labels, att_masks), labels[:, 1:], masks[:, 1:])
+        else:
+            self.model.evel()
+            with torch.no_grad():
+                greedy_res, _ = self.model(fc_feats, att_feats, att_masks, mode='sample')
+            self.model.train()
+            gen_result, sample_logprobs = self.model(fc_feats, att_feats, att_masks, opt={'sample_method': 'sample'}, mode='sample')
+            gts = [gts[_] for _ in gt_indices.tolist()]
+            reward = get_self_critical_reward(greedy_res, gts, gen_result, self.opt)
+            reward = torch.from_numpy(reward).float().to(gen_result.device)
+            loss = self.rl_crit(sample_logprobs, gen_result.data, reward)
+            out['reward'] = reward[:, 0].mean()
+        out['loss'] = loss
+        return out
